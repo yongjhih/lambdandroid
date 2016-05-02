3f89f2ab2322b599759cba842bb0d9b34ed3dc89
Andrew Chen <yongjhih@gmail.com>
2016-05-02 11:29:20 +0800

    Refine LambdaAsyncTask

--- lambdandroid/src/main/java/android/os/LambdaAsyncTask.java
+++ lambdandroid/src/main/java/android/os/LambdaAsyncTask.java
@@ -1,6 +1,10 @@
 package android.os;
 
 public class LambdaAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+    public LambdaAsyncTask() {
+        super();
+    }
+
     @Override
     protected Result doInBackground(Params... params) {
         if (doInBackground == null) return null;
@@ -19,17 +23,17 @@ public class LambdaAsyncTask<Params, Progress, Result> extends AsyncTask<Params,
         onPostExecute(result);
     }
 
-    public LambdaAsyncTask doInBackground(Funcs1<Params, Result> doInBackground) {
+    public LambdaAsyncTask<Params, Progress, Result> doInBackground(Funcs1<Params, Result> doInBackground) {
         this.doInBackground = doInBackground;
         return this;
     }
 
-    public LambdaAsyncTask onProgressUpdate(Actions1<Progress> onProgressUpdate) {
+    public LambdaAsyncTask<Params, Progress, Result> onProgressUpdate(Actions1<Progress> onProgressUpdate) {
         this.onProgressUpdate = onProgressUpdate;
         return this;
     }
 
-    public LambdaAsyncTask onPostExecute(Action1<Result> onPostExecute) {
+    public LambdaAsyncTask<Params, Progress, Result> onPostExecute(Action1<Result> onPostExecute) {
         this.onPostExecute = onPostExecute;
         return this;
     }
@@ -56,12 +60,40 @@ public class LambdaAsyncTask<Params, Progress, Result> extends AsyncTask<Params,
     Actions1<Progress> onProgressUpdate;
     Action1<Result> onPostExecute;
 
+    /*
+    Params[] params;
+
+    public LambdaAsyncTask<Params, ?, ?> params(Params... params) {
+        this.params = params;
+        return this;
+    }
+
+    public static <T> LambdaAsyncTask<T, ?, ?> of(T... t) {
+        return new LambdaAsyncTask<T, Object, Object>().params(t);
+    }
+    */
+
+    /*
+    public static <T, P, R> LambdaAsyncTask<T, P, R> of(Funcs1<? extends T, ? extends R> doInBackground, Actions1<? extends P> onProgressUpdate, Action1<? extends R> onPostExecute) {
+        return LambdaAsyncTask.<T, P, R>on(doInBackground, onProgressUpdate, onPostExecute);
+    }
+    */
+
+    //T[] params;
+    //Funcs1<Params, Result> doInBackground;
+    //Actions1<Progress> onProgressUpdate;
+    //Action1<Result> onPostExecute;
+
     public interface Action1<T> {
         void call(T t);
     }
 
     public interface Actions1<T> {
         void call(T... t);
+    }
+
+    public interface Func0<R> {
+        R call();
     }
 
     public interface Func1<T, R> {
